698c765ea8c1d76fe31b99b4dccbc3ff4ced0300
gizmore <gizmore@wechall.net>
2024-01-31 13:25:35 +0100

    'TOR'

--- Module_TorChallenge.php
+++ Module_TorChallenge.php
@@ -1,10 +1,14 @@
 <?php
 namespace GDO\TorChallenge;
 
+use GDO\Core\GDO;
 use GDO\Core\GDO_Module;
+use GDO\Core\GDO_RedirectError;
 use GDO\Core\GDT_Checkbox;
 use GDO\Core\GDT_String;
 use GDO\Core\GDT_Token;
+use GDO\User\GDO_User;
+use GDO\Util\Random;
 
 final class Module_TorChallenge extends GDO_Module
 {
@@ -25,13 +29,22 @@ final class Module_TorChallenge extends GDO_Module
     {
         return [
             GDT_String::make('eigentor')->initial(t('you_have_to_use_tor')),
-            GDT_Checkbox::make('eigentor')->initial(t('you_have_to_use_tor')),
         ];
     }
 
+    /**
+     * @throws GDO_RedirectError
+     */
     public function hookTorDetected(): void
     {
-        GDT_Token::generateToken()
+        $user = GDO_User::current();
+        if (!($old = $user->settingVar('TorChallenge', 'eigentor')))
+        {
+            $old = Random::randomKey(GDO::TOKEN_LENGTH);
+            $user->saveSettingVar('TorChallenge', 'eigentor', $old);
+            $href = href('TorChallenge', 'Solved');
+            throw new GDO_RedirectError('msg_tor_challenge_solved', [$old], $href);
+        }
     }
 
 
