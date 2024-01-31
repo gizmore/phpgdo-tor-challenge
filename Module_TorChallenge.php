1cfa688636d21a6957994c63b0d80c366529b2a4
gizmore <gizmore@wechall.net>
2024-01-31 13:15:14 +0100

    '7.0.3-r1820 - Safe file_put_contents'

--- Module_TorChallenge.php
+++ Module_TorChallenge.php
@@ -2,16 +2,36 @@
 namespace GDO\TorChallenge;
 
 use GDO\Core\GDO_Module;
+use GDO\Core\GDT_Checkbox;
 use GDO\Core\GDT_String;
+use GDO\Core\GDT_Token;
 
 final class Module_TorChallenge extends GDO_Module
 {
 
+    public function onLoadLanguage(): void
+    {
+        $this->loadLanguage('lang/torchallenge');
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            'TorDetection',
+        ];
+    }
+
     public function getUserConfig(): array
     {
         return [
-            GDT_String::make('eigentor'),
+            GDT_String::make('eigentor')->initial(t('you_have_to_use_tor')),
+            GDT_Checkbox::make('eigentor')->initial(t('you_have_to_use_tor')),
         ];
+    }
+
+    public function hookTorDetected(): void
+    {
+        GDT_Token::generateToken()
     }
 
 
